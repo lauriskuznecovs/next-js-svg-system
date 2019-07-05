b4b78cdf1f61f829196e86cd93a2f5d1ebed030d
lauris-kuznecovs <lauris.kuznecovs@wunder.io>
2019-07-05 18:03:52 +0300

    Cleanup un-needed stuff.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-// import PropTypes from 'prop-types'
 
 import App from '../components/App'
 import Examples from '../components/Examples'
@@ -10,12 +9,7 @@ export default class Index extends Component {
     return query
   }
 
-  // static propTypes = {
-  //   langcode: PropTypes.string.isRequired,
-  // }
-
   render () {
-    // const { langcode } = this.props
     return (
       <App {...this.props}>
         <Examples />
