e70e3f86911590a51cb6e1ae47a65ab6d8aa3469
lauris-kuznecovs <lauris.kuznecovs@wunder.io>
2019-07-05 17:59:02 +0300

    Theme cleanup.

--- themes/theme.js
+++ themes/theme.js
@@ -20,72 +20,9 @@ theme.palette = {
 }
 
 theme.fonts = {
-  primary: 'Signika Negative, sans-serif', // titles, headers
-  secondary: 'Source Sans Pro, sans-serif', // body, labels, buttons etc.
+  primary: 'sans-serif',
 }
 
-theme.typography = {
-  h1: {
-    weight: 600,
-    mobile: {
-      size: '2.5em', // 40px
-      lineHeight: '2.875rem', // 46px
-    },
-    desktop: {
-      size: '2.5em', // 40px
-      lineHeight: '3.3333rem', // 50px
-    },
-  },
-  h2: {
-    weight: 600,
-    mobile: {
-      size: '2.25em', // 36px
-      lineHeight: '2.5rem',
-    },
-    desktop: {
-      size: '2.5em', // 40px
-      lineHeight: '1em',
-    },
-  },
-  h3: {
-    weight: 600,
-    mobile: {
-      size: '1.8125em', // 29px
-      lineHeight: '2.1875em', // 35px
-    },
-    desktop: {
-      size: '1.75em', // 28px
-      lineHeight: '2.125em', // 34px
-    },
-  },
-  h4: {
-    weight: 600,
-    mobile: {
-      size: '1.625em', // 26px
-      lineHeight: '2em', // 32px
-    },
-    desktop: {
-      size: '1.5em', // 24px
-      lineHeight: '1.875em', // 30px
-    },
-  },
-  h5: {
-    weight: 600,
-    mobile: {
-      size: '1.4375em', // 23px
-      lineHeight: '1.75em', // 28px
-    },
-    desktop: {
-      size: '1.25em', // 20px
-      lineHeight: '1.625em', // 26px
-    },
-  },
-  mainContent: '1.125rem',
-  readMoreLink: {
-    weight: 600,
-    size: '1.25em', // 20px
-    lineHeight: '1.25em',
-  },
-}
+theme.typography = {}
 
 export default theme
